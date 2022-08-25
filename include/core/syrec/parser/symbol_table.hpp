87fd9deb27444974ac9a671ac7023fd0dd8ca636
Fabian Hingerl <fabian.hingerl@gmail.com>
2022-08-25 23:32:54 +0200

    Implemented symbol_table functions (basic draft), added some checks that invalid AST objects do not get generated in ATG

--- include/core/syrec/parser/symbol_table.hpp
+++ include/core/syrec/parser/symbol_table.hpp
@@ -9,26 +9,32 @@
 namespace syrec {
     class symbol_table {
     public:
+        typedef std::shared_ptr<symbol_table> ptr;
+
         symbol_table() {
             locals = {};
+            modules = {};
             outer            = nullptr;
         }
 
         ~symbol_table() = default;
 
-        bool                              contains(const std::string_view &literal);
+        bool                              contains(const std::string &literal);
         bool                              contains(const module::ptr& module);
-        [[nodiscard]] const variable::ptr get_variable(const std::string_view &literal);
-        [[nodiscard]] const module::ptr   get_module(const std::string_view &moduleName);
+        [[nodiscard]] variable::ptr get_variable(const std::string &literal);
         void                              add_entry(const variable::ptr& local_entry);
         void                              add_entry(const module::ptr& module);                     
 
-        void open_scope();
-        void close_scope();
+        static void open_scope(symbol_table::ptr &current_scope);
+        static void close_scope(symbol_table::ptr &current_scope);
 
     private:
         std::map<std::string, variable::ptr> locals;
-        std::shared_ptr<symbol_table>      outer;
+        std::map<std::string, std::vector<module::ptr>> modules; 
+        symbol_table::ptr      outer;
+
+        static bool do_module_signatures_match(const module::ptr& module, const module::ptr& other_module);
+        static bool are_parameters_equal(const variable::ptr& parameter_one, const variable::ptr& parameter_two);
     };    
 }
 
--- src/core/syrec/parser/symbol_table.cpp
+++ src/core/syrec/parser/symbol_table.cpp
@@ -1,24 +1,40 @@
 #include "core/syrec/parser/symbol_table.hpp"
 
+#include <functional>
+
 namespace syrec {
-    bool                              symbol_table::contains(const std::string_view& literal) {
-        // TODO: Implement me
-        return false;
+    bool                              symbol_table::contains(const std::string& literal) {
+        return this->locals.find(literal) != this->locals.end();
     }
 
     bool                              symbol_table::contains(const module::ptr& module) {
-        // TODO: Implement me
-        return false;
+        if (nullptr == module) {
+            return false;
+        }
+
+        bool       found_module_matching_signature = false;
+        const auto module_search_result = this->modules.find(module->name);
+        if (module_search_result != this->modules.end()) {
+            const std::vector<module::ptr> modules_with_matching_name = module_search_result->second;
+
+            // TODO: Could replace with std::find_if(...)
+            for (size_t i = 0; i < modules_with_matching_name.size() && !found_module_matching_signature; i++) {
+                found_module_matching_signature = do_module_signatures_match(modules_with_matching_name.at(i), module);
+            }
+        }
+
+        return found_module_matching_signature;
     }
 
-    [[nodiscard]] const variable::ptr symbol_table::get_variable(const std::string_view& literal) {
-        // TODO: Implement me
-        return {};
-    }
+    [[nodiscard]] variable::ptr symbol_table::get_variable(const std::string& literal) {
 
-    [[nodiscard]] const module::ptr   get_module(const std::string_view& moduleName) {
-        // TODO: Implement me
-        return {};
+        if (contains(literal)) {
+            return this->locals.find(literal)->second;
+        } else if (nullptr != outer) {
+            return outer->get_variable(literal);
+        } else {
+            return nullptr;
+        }
     }
 
     void                              symbol_table::add_entry(const variable::ptr& local_entry) {
@@ -29,11 +45,36 @@ namespace syrec {
         // TODO: Implement me
     }
 
-    void symbol_table::open_scope() {
+    void symbol_table::open_scope(symbol_table::ptr &current_scope) {
         // TODO: Implement me
+        const symbol_table::ptr new_scope(new symbol_table());
+        new_scope->outer = current_scope;
+        current_scope    = new_scope;
     }
 
-    void symbol_table::close_scope() {
-        // TODO: Implement me
+    void symbol_table::close_scope(symbol_table::ptr &current_scope) {
+        const symbol_table::ptr &outer_scope = current_scope->outer;
+        if (nullptr != outer_scope) {
+            current_scope->outer = nullptr;
+        }
+        current_scope = outer_scope;
+    }
+
+    [[nodiscard]] bool symbol_table::do_module_signatures_match(const module::ptr& module, const module::ptr& other_module) {
+        bool signatures_match = module->parameters.size() == other_module->parameters.size();
+        for (size_t param_idx = 0; param_idx < module->parameters.size() && signatures_match; param_idx++) {
+            const variable::ptr &parameter_first_module = module->parameters.at(param_idx);
+            const variable::ptr &parameter_other_module = other_module->parameters.at(param_idx);
+            signatures_match                             = are_parameters_equal(parameter_first_module, parameter_other_module);
+        }
+        return signatures_match;
+    }
+    
+    [[nodiscard]] bool symbol_table::are_parameters_equal(const variable::ptr& parameter_one, const variable::ptr& parameter_two) {
+        return parameter_one->type == parameter_two->type
+            && parameter_one->name == parameter_two->name
+            && parameter_one->dimensions == parameter_two->dimensions
+            && parameter_one->bitwidth == parameter_two->bitwidth;
+    
     }
 }
