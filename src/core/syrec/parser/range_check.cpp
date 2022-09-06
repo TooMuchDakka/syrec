fda3667aaf5112065009bfc27eb24106aad6843b
Fabian Hingerl <fabian.hingerl@gmail.com>
2022-09-06 21:30:20 +0200

    Updated checks when fetching valid range of values for dimension of signal

--- src/core/syrec/parser/range_check.cpp
+++ src/core/syrec/parser/range_check.cpp
@@ -3,19 +3,31 @@
 namespace range_check {
     static std::optional<valid_index_access_range> get_valid_dimension_access_range(const syrec::variable::ptr& variable, const std::size_t dimension_idx, const bool zero_based_indexing) {
         std::optional<valid_index_access_range> valid_index_range;
-        if ((zero_based_indexing && dimension_idx >= variable->dimensions.size()) || (!zero_based_indexing && dimension_idx > variable->dimensions.size())) {
-            return valid_index_range;
-            // TODO: Throw exception
+
+        std::size_t max_valid_dimension_value;
+        if (zero_based_indexing) {
+            max_valid_dimension_value = variable->dimensions.empty() ? 0 : variable->dimensions.size() - 1;
+        } else {
+            max_valid_dimension_value = variable->dimensions.empty() ? 1 : variable->dimensions.size();
         }
 
-        std::size_t min_valid_dimension_value, max_valid_dimension_value;
-        if (zero_based_indexing) {
-            min_valid_dimension_value = 0;
-            max_valid_dimension_value = variable->dimensions.size() - 1;
-        } else {
-            min_valid_dimension_value = 1;
-            max_valid_dimension_value = variable->dimensions.size();
+        if (dimension_idx > max_valid_dimension_value) {
+            // TODO: Throw exception
+            return valid_index_range;
         }
+
+        const std::size_t min_valid_dimension_value = zero_based_indexing ? 0 : 1;
+        if (variable->dimensions.empty()) {
+            max_valid_dimension_value = min_valid_dimension_value;
+        }
+        else {
+            if (zero_based_indexing) {
+                max_valid_dimension_value = variable->dimensions.at(dimension_idx) - 1;        
+            } else {
+                max_valid_dimension_value = variable->dimensions.at(dimension_idx);
+            }
+        }
+
         valid_index_range.emplace(valid_index_access_range(min_valid_dimension_value, max_valid_dimension_value));
         return valid_index_range;
     }
