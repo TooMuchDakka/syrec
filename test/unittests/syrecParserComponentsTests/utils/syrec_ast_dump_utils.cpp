8eafaef21ed3ea1cf60dff795c60252c68ca19d2
Fabian Hingerl <fabian.hingerl@gmail.com>
2022-10-11 21:42:37 +0200

    Added templates for easier stringification of sequence of elements of same type in AST, added implementations for stringification of module, variable, variableAccess, number and statement[s]

--- test/unittests/syrecParserComponentsTests/utils/syrec_ast_dump_utils.cpp
+++ test/unittests/syrecParserComponentsTests/utils/syrec_ast_dump_utils.cpp
@@ -3,14 +3,46 @@
 using namespace syrecAstDumpUtils;
 
 std::string syrecAstDumpUtils::stringifyModules(const syrec::Module::vec& modules) {
-    return "";
+    return stringifyAndJoinMany(modules, "\n", stringifyModule);
 }
 
 std::string syrecAstDumpUtils::stringifyModule(const syrec::Module::ptr& moduleToStringify) {
-    return "";
+    const std::string stringifiedParameters = stringifyAndJoinMany(moduleToStringify->parameters, ",", stringifyVariable);
+    const std::string stringifiedLocals     = stringifyAndJoinMany(moduleToStringify->variables, ",", stringifyVariable);
+    const std::string stringifiedStmts      = stringifyStatements(moduleToStringify->statements);
+
+    return "module " + moduleToStringify->name + "(" + stringifiedParameters + ")\n" + stringifiedLocals + "\n" + stringifiedStmts;
+}
+
+std::string syrecAstDumpUtils::stringifyStatements(const syrec::Statement::vec& statements) {
+    return stringifyAndJoinMany(statements, ";", stringifyStatement);
 }
 
 std::string syrecAstDumpUtils::stringifyStatement(const syrec::Statement::ptr& statement) {
+    if (auto const* swapStmt = dynamic_cast<syrec::SwapStatement*>(statement.get())) {
+        return stringifySwapStatement(*swapStmt);
+    }
+    if (auto const* assignStmt = dynamic_cast<syrec::AssignStatement*>(statement.get())) {
+        return stringifyAssignStatement(*assignStmt);
+    }
+    if (auto const* callStmt = dynamic_cast<syrec::CallStatement*>(statement.get())) {
+        return stringifyCallStatement(*callStmt);
+    }
+    if (auto const* uncallStmt = dynamic_cast<syrec::UncallStatement*>(statement.get())) {
+        return stringifyUncallStatement(*uncallStmt);
+    }
+    if (auto const* unaryStmt = dynamic_cast<syrec::UnaryStatement*>(statement.get())) {
+        return stringifyUnaryStatement(*unaryStmt);
+    }
+    if (auto const* skipStmt = dynamic_cast<syrec::SkipStatement*>(statement.get())) {
+        return stringifySkipStatement(*skipStmt);
+    }
+    if (auto const* forStmt = dynamic_cast<syrec::ForStatement*>(statement.get())) {
+        return stringifyForStatement(*forStmt);
+    }
+    if (auto const* ifStmt = dynamic_cast<syrec::IfStatement*>(statement.get())) {
+        return stringifyIfStatement(*ifStmt);
+    }
     return "";
 }
 
@@ -50,30 +82,74 @@ std::string syrecAstDumpUtils::stringifyExpression(const syrec::expression::ptr&
     return "";
 }
 
-std::string syrecAstDumpUtils::stringifyBinaryExpression(const syrec::BinaryExpression::ptr& binaryExpr) {
+std::string syrecAstDumpUtils::stringifyBinaryExpression(const syrec::BinaryExpression& binaryExpr) {
     return "";
 }
 
-std::string syrecAstDumpUtils::stringifyNumericExpression(const syrec::NumericExpression::ptr& numericExpr) {
+std::string syrecAstDumpUtils::stringifyNumericExpression(const syrec::NumericExpression& numericExpr) {
     return "";
 }
 
-std::string syrecAstDumpUtils::stringifyShiftExpression(const syrec::ShiftExpression::ptr& shiftExpr) {
+std::string syrecAstDumpUtils::stringifyShiftExpression(const syrec::ShiftExpression& shiftExpr) {
     return "";
 }
 
-std::string syrecAstDumpUtils::stringifyVariableExpression(const syrec::VariableExpression&& variableExpression) {
+std::string syrecAstDumpUtils::stringifyVariableExpression(const syrec::VariableExpression& variableExpression) {
     return "";
 }
 
-std::string syrecAstDumpUtils::stringifyVariable(const syrec::Variable& variable) {
-    return "";
+// TODO: Throw exception on invalid signal type ?
+std::string syrecAstDumpUtils::stringifyVariable(const syrec::Variable::ptr& variable) {
+    const std::string dimensionsStringified = stringifyAndJoinMany<unsigned int>(variable->dimensions, "", [](const unsigned int& t) { return "[" + std::to_string(t) + "]"; });
+    const std::string bitwidthStringified   = "(" + std::to_string(variable->bitwidth) + ")";
+
+    std::string variableAssignabilityStringified;
+    switch (variable->type) {
+        case syrec::Variable::In:
+            variableAssignabilityStringified = "in";
+            break;
+        case syrec::Variable::Out:
+            variableAssignabilityStringified = "out";
+            break;
+        case syrec::Variable::Inout:
+            variableAssignabilityStringified = "inout";
+            break;
+        case syrec::Variable::Wire:
+            variableAssignabilityStringified = "wire";
+            break;
+        case syrec::Variable::State:
+            variableAssignabilityStringified = "state";
+            break;
+        default:
+            variableAssignabilityStringified = "invalid";
+            break;
+    }
+    return variableAssignabilityStringified + " " + variable->name + dimensionsStringified + bitwidthStringified;
 }
 
-std::string syrecAstDumpUtils::stringifyVariableAccess(const syrec::VariableAccess& variableAccess) {
-    return "";
+std::string syrecAstDumpUtils::stringifyVariableAccess(const syrec::VariableAccess::ptr& variableAccess) {
+    std::string accessedDimension;
+    std::string bitAccess;
+
+    if (!variableAccess->indexes.empty()) {
+       accessedDimension = stringifyAndJoinManyComplex<syrec::expression>(variableAccess->indexes, "", [](const syrec::expression::ptr& expr) { return "[" + stringifyExpression(expr) + "]"; });
+    }
+
+    if (variableAccess->range.has_value()) {
+        const auto&       definedBitAccess = variableAccess->range.value();
+        bitAccess                    = "." + stringifyNumber(definedBitAccess.first);
+
+        if (definedBitAccess.first != definedBitAccess.second) {
+            bitAccess += ":" + stringifyNumber(definedBitAccess.second);
+        }
+    }
+    return variableAccess->var->name + accessedDimension + bitAccess;
 }
 
-std::string syrecAstDumpUtils::stringifyNumber(const syrec::Number& number) {
-    return "";
+// TODO: Handling of loop variable
+std::string syrecAstDumpUtils::stringifyNumber(const syrec::Number::ptr& number) {
+    if (number->isLoopVariable()) {
+        return "$" + number->variableName();
+    }
+    return std::to_string(number->evaluate({}));
 }
--- test/unittests/syrecParserComponentsTests/utils/syrec_ast_dump_utils.hpp
+++ test/unittests/syrecParserComponentsTests/utils/syrec_ast_dump_utils.hpp
@@ -2,16 +2,42 @@
 #define SYREC_AST_DUMP_UTILS
 #pragma once
 
+#include "algorithm"
+#include "sstream"
+
 #include "core/syrec/module.hpp"
 
 namespace syrecAstDumpUtils {
+    template<class T>
+    std::string stringifyAndJoinMany(const std::vector<T>& elements, const char* delimiter, std::string (*stringifyFunc)(const T& t)) {
+        if (elements.empty()) {
+            return "";
+        }
+
+        std::ostringstream       resultBuffer{};
+        std::vector<std::string> stringifiedElements{};
+
+        std::transform(
+                elements.cbegin(),
+                elements.cend(),
+                std::back_inserter(stringifiedElements),
+                [stringifyFunc](const T& elem) { return stringifyFunc(elem); });
+
+        std::copy(stringifiedElements.cbegin(), stringifiedElements.cend(), std::ostream_iterator<std::string>(resultBuffer, delimiter));
+        return resultBuffer.str();
+    }
+
+    template<class T>
+    const auto stringifyAndJoinManyComplex = stringifyAndJoinMany<std::shared_ptr<T>>;
+
     [[nodiscard]] std::string stringifyModules(const syrec::Module::vec& modules);
     [[nodiscard]] std::string stringifyModule(const syrec::Module::ptr& moduleToStringify);
 
+    [[nodiscard]] std::string stringifyStatements(const syrec::Statement::vec& statements);
     [[nodiscard]] std::string stringifyStatement(const syrec::Statement::ptr& statement);
     [[nodiscard]] std::string stringifySwapStatement(const syrec::SwapStatement& swapStmt);
     [[nodiscard]] std::string stringifyAssignStatement(const syrec::AssignStatement& assignStmt);
-    [[nodiscard]] std::string stringifyCallStatement(const syrec::CallStatement &callStmt);
+    [[nodiscard]] std::string stringifyCallStatement(const syrec::CallStatement& callStmt);
     [[nodiscard]] std::string stringifyUncallStatement(const syrec::UncallStatement& uncallStmt);
     [[nodiscard]] std::string stringifyUnaryStatement(const syrec::UnaryStatement& unaryStmt);
     [[nodiscard]] std::string stringifySkipStatement(const syrec::SkipStatement& skipStmt);
@@ -19,14 +45,14 @@ namespace syrecAstDumpUtils {
     [[nodiscard]] std::string stringifyIfStatement(const syrec::IfStatement& ifStmt);
 
     [[nodiscard]] std::string stringifyExpression(const syrec::expression::ptr& expression);
-    [[nodiscard]] std::string stringifyBinaryExpression(const syrec::BinaryExpression::ptr& binaryExpr);
-    [[nodiscard]] std::string stringifyNumericExpression(const syrec::NumericExpression::ptr& numericExpr);
-    [[nodiscard]] std::string stringifyShiftExpression(const syrec::ShiftExpression::ptr& shiftExpr);
-    [[nodiscard]] std::string stringifyVariableExpression(const syrec::VariableExpression&& variableExpression);
+    [[nodiscard]] std::string stringifyBinaryExpression(const syrec::BinaryExpression& binaryExpr);
+    [[nodiscard]] std::string stringifyNumericExpression(const syrec::NumericExpression& numericExpr);
+    [[nodiscard]] std::string stringifyShiftExpression(const syrec::ShiftExpression& shiftExpr);
+    [[nodiscard]] std::string stringifyVariableExpression(const syrec::VariableExpression& variableExpression);
 
-    [[nodiscard]] std::string stringifyVariable(const syrec::Variable& variable);
-    [[nodiscard]] std::string stringifyVariableAccess(const syrec::VariableAccess& variableAccess);
-    [[nodiscard]] std::string stringifyNumber(const syrec::Number& number);
+    [[nodiscard]] std::string stringifyVariable(const syrec::Variable::ptr& variable);
+    [[nodiscard]] std::string stringifyVariableAccess(const syrec::VariableAccess::ptr& variableAccess);
+    [[nodiscard]] std::string stringifyNumber(const syrec::Number::ptr& number);
 }
 
 #endif
