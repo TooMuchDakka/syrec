e2dea9ef365457f89b13513bda88a958b4065e54
Fabian Hingerl <fabian.hingerl@gmail.com>
2023-03-29 15:42:19 +0200

    Added implementation for potential value storage and updated interface to also extract only parts of the stored value

--- include/core/syrec/parser/optimizations/constantPropagation/potential_value_storage.hpp
+++ include/core/syrec/parser/optimizations/constantPropagation/potential_value_storage.hpp
@@ -2,19 +2,39 @@
 #define POTENTIAL_VALUE_STORAGE_HPP
 #pragma once
 
+#include "core/syrec/parser/optimizations/constantPropagation/bit_range_access_restriction.hpp"
+
 #include <variant>
 #include <vector>
 #include <optional>
 
 namespace optimizations {
     struct PotentialValueStorage {
+        enum LayerType {
+            Intermediate,
+            Value
+        };
+
+        const LayerType                                                               layerType;
         std::vector<std::variant<PotentialValueStorage, std::optional<unsigned int>>> layerData;
 
-        void tryInvalidateStoredValue();
+        explicit PotentialValueStorage() : layerType(Intermediate), layerData({}) {}
+        explicit PotentialValueStorage(const LayerType type, std::vector<std::variant<PotentialValueStorage, std::optional<unsigned int>>> data) : layerType(type), layerData(std::move(data)) {}
+
         [[nodiscard]] static PotentialValueStorage initAsIntermediateLayer(unsigned int entries);
         [[nodiscard]] static PotentialValueStorage initAsValueLookupLayer(unsigned int numEntries, unsigned int defaultValue);
+        
+        void tryUpdateStoredValue(unsigned int layerDataEntryIdx, unsigned int newValue);
+        void tryUpdatePartOfStoredValue(unsigned int layerDataEntryIdx, unsigned int newValue, const BitRangeAccessRestriction::BitRangeAccess& partsToUpdate);
 
-        void invalidateCurrentAndSubsequentLayers();
+        void                                      tryInvalidateStoredValue(const std::optional<unsigned int>& layerDataEntryIdx);
+        [[nodiscard]] std::optional<unsigned int> tryFetchStoredValue(unsigned int layerDataEntryIdx, const std::optional<BitRangeAccessRestriction::BitRangeAccess>& bitRangeToFetch) const;
+
+    private:
+        [[nodiscard]] static std::vector<std::variant<PotentialValueStorage, std::optional<unsigned int>>> createLayerData(std::size_t numEntries, unsigned int defaultValue);
+        [[nodiscard]] static std::vector<std::variant<PotentialValueStorage, std::optional<unsigned int>>> createLayerData(std::size_t numEntries);
+
+        [[nodiscard]] static unsigned int extractPartsOfValue(unsigned int value, const BitRangeAccessRestriction::BitRangeAccess& partToFetch);
     };
 };
 #endif
