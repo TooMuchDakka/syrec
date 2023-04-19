199741e0bf43ecfb239ac2200382ca05fee8cc84
Fabian Hingerl <fabian.hingerl@gmail.com>
2023-04-19 22:33:50 +0200

    Added test fixture and test case source for constant propagation optimizations

---
+++ test/unittests/syrecOptimizerComponentsTests/test_constant_propagation_optimization.cpp
@@ -0,0 +1,72 @@
+#include "core/syrec/program.hpp"
+#include "syrec_ast_dump_utils.hpp"
+#include "test_case_creation_utils.hpp"
+
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using namespace syrec;
+
+class ConstantPropagationTests: public testing::TestWithParam<std::string> {
+public:
+    inline static const std::string pathToTestCaseFile = "./unittests/syrecOptimizerComponentsTests/testdata/test_constant_propagation_optimization.json";
+
+private:
+    const std::string cJsonKeyCircuit               = "circuit";
+    const std::string cJsonKeyExpectedCircuitOutput = "expectedCircuit";
+
+protected:
+    syrecAstDumpUtils::SyrecASTDumper astDumper;
+    syrec::ReadProgramSettings        config;
+    syrec::program                    parserPublicInterface;
+
+    std::string circuitToOptimize;
+    std::string expectedOptimizedCircuit;
+
+    explicit ConstantPropagationTests():
+        astDumper(syrecAstDumpUtils::SyrecASTDumper(true)), config(16, false, false, true) {}
+
+    void SetUp() override {
+        const std::string testCaseJsonKey = GetParam();
+
+        std::ifstream configFileStream(ConstantPropagationTests::pathToTestCaseFile, std::ios_base::in);
+        ASSERT_TRUE(configFileStream.good()) << "Could not open test data json file @ "
+                                             << ConstantPropagationTests::pathToTestCaseFile;
+
+        const nlohmann::json parsedJson = nlohmann::json::parse(configFileStream);
+        ASSERT_TRUE(parsedJson.contains(testCaseJsonKey)) << "Required entry for given test case with key '" << testCaseJsonKey << "' was not found";
+
+        const nlohmann::json testcaseJsonData = parsedJson[testCaseJsonKey];
+        ASSERT_TRUE(testcaseJsonData.is_object()) << "Expected test case data for test with key '" << testCaseJsonKey << "' to be in the form of a json object";
+
+        ASSERT_TRUE(testcaseJsonData.contains(cJsonKeyCircuit)) << "Required entry with key '" << cJsonKeyCircuit << "' was not found";
+        ASSERT_TRUE(testcaseJsonData.at(cJsonKeyCircuit).is_string()) << "Expected entry with key '" << cJsonKeyCircuit << "' to by a string";
+        circuitToOptimize = testcaseJsonData.at(cJsonKeyCircuit).get<std::string>();
+
+        if (testcaseJsonData.contains(cJsonKeyExpectedCircuitOutput)) {
+            ASSERT_TRUE(testcaseJsonData.at(cJsonKeyExpectedCircuitOutput).is_string()) << "Expected entry with key '" << cJsonKeyExpectedCircuitOutput << "' to by an array";
+            expectedOptimizedCircuit = testcaseJsonData.at(cJsonKeyExpectedCircuitOutput).get<std::string>();
+        } else {
+            expectedOptimizedCircuit = circuitToOptimize;
+        }
+    }
+};
+
+INSTANTIATE_TEST_SUITE_P(SyReCOptimizations, ConstantPropagationTests,
+                         testing::ValuesIn(syrecTestUtils::loadTestCasesNamesFromFile(ConstantPropagationTests::pathToTestCaseFile, {})),
+                         [](const testing::TestParamInfo<ConstantPropagationTests::ParamType>& info) {
+                             auto s = info.param;
+                             std::replace( s.begin(), s.end(), '-', '_');
+                             return s; });
+
+TEST_P(ConstantPropagationTests, GenericConstantPropagationOptimization) {
+    std::string errorsFromParsedCircuit;
+    ASSERT_NO_THROW(errorsFromParsedCircuit = parserPublicInterface.readFromString(circuitToOptimize, config));
+    ASSERT_TRUE(errorsFromParsedCircuit.empty()) << "Expected to be able to parse given circuit without errors";
+
+    std::string stringifiedProgram;
+    ASSERT_NO_THROW(stringifiedProgram = astDumper.stringifyModules(parserPublicInterface.modules())) << "Failed to stringify parsed modules";
+    ASSERT_EQ(expectedOptimizedCircuit, stringifiedProgram);
+}
