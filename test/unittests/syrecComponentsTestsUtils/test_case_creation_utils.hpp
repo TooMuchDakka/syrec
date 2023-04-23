fb3dcd7cc62ace58aa219326c79ecc95e45b285b
Fabian Hingerl <fabian.hingerl@gmail.com>
2023-04-23 15:13:51 +0200

    Test case that do not conform to the gtest test case name convention will be ignored (by creating a test case indicating faulty name) instead of simply omitting creating a test case

--- test/unittests/syrecComponentsTestsUtils/test_case_creation_utils.hpp
+++ test/unittests/syrecComponentsTestsUtils/test_case_creation_utils.hpp
@@ -10,6 +10,19 @@
 #include <nlohmann/json.hpp>
 
 namespace syrecTestUtils {
+    /*
+     * Only non-empty ASCII alphanumeric characters without underscores are supported as tests names
+     * see https://google.github.io/googletest/advanced.html#specifying-names-for-value-parameterized-test-parameters
+     */ 
+    inline bool doesStringContainOnlyAsciiCharactersAndNoUnderscores(const std::string_view& stringToCheck) {
+        return std::all_of(
+                stringToCheck.cbegin(),
+                stringToCheck.cend(),
+                [](const char characterToCheck) {
+                    return std::isalnum(characterToCheck);
+                });
+    }
+
     inline std::vector<std::string> loadTestCasesNamesFromFile(const std::string& testDataJsonFilename, const std::vector<std::string>& testCasesToIgnore) {
         std::ifstream testDataFileStream(testDataJsonFilename, std::ios_base::in);
         if (!testDataFileStream.good()) {
@@ -26,13 +39,23 @@ namespace syrecTestUtils {
 
         const std::set<std::string> setOfTestCasesToIgnore(testCasesToIgnore.cbegin(), testCasesToIgnore.cend());
         std::vector<std::string>    foundTestCases;
+        std::size_t                 testCaseIdx = 0;
         for (const auto& testCaseKVPair: parsedJson.items()) {
             const auto& testCaseName = testCaseKVPair.key();
-            if (setOfTestCasesToIgnore.count(testCaseName) != 0) {
-                foundTestCases.emplace_back("DISABLED_" + testCaseName);
-            } else {
-                foundTestCases.emplace_back(testCaseName);
+            if (testCaseName.empty()) {
+                continue;
             }
+
+            if (doesStringContainOnlyAsciiCharactersAndNoUnderscores(testCaseName)) {
+                if (setOfTestCasesToIgnore.count(testCaseName) != 0) {
+                    foundTestCases.emplace_back("DISABLED_" + testCaseName);
+                } else {
+                    foundTestCases.emplace_back(testCaseName);
+                }   
+            } else {
+                foundTestCases.emplace_back("DISABLED_Test" + std::to_string(testCaseIdx) + "ContainedNonAsciiOrUnderscoreCharacter");
+            }
+            testCaseIdx++;
         }
         return foundTestCases;
     }
